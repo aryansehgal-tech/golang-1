b13c6b99907501646d99fa82c4d8b29aee94c3fd
ryanaPc <aryansehgal3212@gmail.com>
2026-02-15 23:00:44 +0530

    Learning Controllers

--- gin_tutorial/main.go
+++ gin_tutorial/main.go
@@ -1,106 +1,107 @@
 package main
 
 import (
-	"net/http"
+	// "net/http"
+
+	"go-tutorial/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H {
-			"message": "pong from gin",
-		})
-	})
+	// router.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H {
+	// 		"message": "pong from gin",
+	// 	})
+	// })
 
-	router.GET("/me/:id/:newId", func(c *gin.Context) {
-		var id = c.Param("id")
-		var new_id = c.Param("newId")
-		c.JSON(http.StatusOK, gin.H {
-			"user_id": id,
-			"user_new_id": new_id,
-		})
-	})
+	// router.GET("/me/:id/:newId", func(c *gin.Context) {
+	// 	var id = c.Param("id")
+	// 	var new_id = c.Param("newId")
+	// 	c.JSON(http.StatusOK, gin.H {
+	// 		"user_id": id,
+	// 		"user_new_id": new_id,
+	// 	})
+	// })
 
-	router.POST("/me", func(c *gin.Context){
-		// Email, Password
+	// router.POST("/me", func(c *gin.Context){
 
-		type MeRequest struct {
-			Email string `json:"email" binding:"required"`
-			Password string `json: "password"`
-		}
-		var meRequest MeRequest
+	// 	type MeRequest struct {
+	// 		Email string `json:"email" binding:"required"`
+	// 		Password string `json: "password"`
+	// 	}
+	// 	var meRequest MeRequest
 
-		if err := c.BindJSON(&meRequest); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
+	// 	if err := c.BindJSON(&meRequest); err != nil {
+	// 		c.JSON(http.StatusBadRequest, gin.H{
+	// 			"error": err.Error(),
+	// 		})
+	// 		return
+	// 	}
 
-		c.JSON(http.StatusOK, gin.H{
-			"email": meRequest.Email,
-			"password": meRequest.Password,
-		})
-	})
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"email": meRequest.Email,
+	// 		"password": meRequest.Password,
+	// 	})
+	// })
 
-	router.PUT("/me", func(c *gin.Context){
-		// Email, Password
+	// router.PUT("/me", func(c *gin.Context){
 
-		type MeRequest struct {
-			Email string `json:"email" binding:"required"`
-			Password string `json: "password"`
-		}
-		var meRequest MeRequest
+	// 	type MeRequest struct {
+	// 		Email string `json:"email" binding:"required"`
+	// 		Password string `json: "password"`
+	// 	}
+	// 	var meRequest MeRequest
 
-		if err := c.BindJSON(&meRequest); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
+	// 	if err := c.BindJSON(&meRequest); err != nil {
+	// 		c.JSON(http.StatusBadRequest, gin.H{
+	// 			"error": err.Error(),
+	// 		})
 
-			return
-		}
+	// 		return
+	// 	}
 
-		c.JSON(http.StatusOK, gin.H{
-			"email": meRequest.Email,
-			"password": meRequest.Password,
-		})
-	})
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"email": meRequest.Email,
+	// 		"password": meRequest.Password,
+	// 	})
+	// })
 
-	router.PATCH("/me", func(c *gin.Context){
-		// Email, Password
+	// router.PATCH("/me", func(c *gin.Context){
 
-		type MeRequest struct {
-			Email string `json:"email" binding:"required"`
-			Password string `json: "password"`
-		}
-		var meRequest MeRequest
+	// 	type MeRequest struct {
+	// 		Email string `json:"email" binding:"required"`
+	// 		Password string `json: "password"`
+	// 	}
+	// 	var meRequest MeRequest
 
-		if err := c.BindJSON(&meRequest); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
+	// 	if err := c.BindJSON(&meRequest); err != nil {
+	// 		c.JSON(http.StatusBadRequest, gin.H{
+	// 			"error": err.Error(),
+	// 		})
 
-			return
-		}
+	// 		return
+	// 	}
 
-		c.JSON(http.StatusOK, gin.H{
-			"email": meRequest.Email,
-			"password": meRequest.Password,
-		})
-	})
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"email": meRequest.Email,
+	// 		"password": meRequest.Password,
+	// 	})
+	// })
 
-	router.DELETE("/me/:id", func(c *gin.Context){
-		// Email, Password
+	// router.DELETE("/me/:id", func(c *gin.Context){
 		
-		var id = c.Param("id")
-		c.JSON(http.StatusOK, gin.H{
-			"id":id,
-			"message":"Deleted",
-		})
+	// 	var id = c.Param("id")
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"id":id,
+	// 		"message":"Deleted",
+	// 	})
 		
-	})
+	// })
+
+	notesController := &controllers.NotesController{}
+	notesController.InitNotesControllerRoutes(router)
 
 	router.Run()
 }
